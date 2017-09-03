49ba533d1ddd555a3956f332d2a81ec298d2492e
Eddie <eddiemonroe@gmail.com>
2017-09-03 15:59:32 +0800

    changes to run.sh for running on local head stack

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ export LOVING_AI_WORKSPACE=$BASEDIR
 export HR_CHARACTER_PATH=$LOVING_AI_WORKSPACE/chatbot-server/${BOTNAME}.yaml
 export CHATBOT_SERVER_PORT=8002
 export CS_DIR=$LOVING_AI_WORKSPACE/ChatScript-engine
-export CS_PORT=1025
+export CS_PORT=1024
 export SLACKBOT_API_TOKEN=$LOVING_AI_SLACKBOT_API_TOKEN
 export SLACKTEST_TOKEN=$LOVING_AI_SLACKTEST_TOKEN
 export CHATBOT_LOG_DIR=$HOME/.loving-ai
@@ -32,8 +32,5 @@ if [[ ! -d $CS_DIR ]]; then
     git clone git@github.com:hansonrobotics/ChatScript-engine.git $CS_DIR
 fi
 
-tmux new-session -d -n "Loving AI" "echo $HR_CHARACTER_PATH && cd $LOVING_AI_WORKSPACE/chatbot-server && python run_server.py -p $CHATBOT_SERVER_PORT -v; $SHELL"
-tmux new-window -n "CS" "cd $CS_DIR && ../build.exp Sarah && ./run.sh --users ../users --logs ../logs --topic ../topic --tmp ../tmp -p $CS_PORT; $SHELL"
-tmux new-window -n "Slack" "cd $LOVING_AI_WORKSPACE/chatbot-server && python slack_client.py $SLACK_BOTNAME; $SHELL"
+tmux new-window -n "Loving-AI CS" "cd $CS_DIR && ../build.exp Sarah && ./run.sh --users ../users --logs ../logs --topic ../topic --tmp ../tmp -p $CS_PORT; $SHELL"
 tmux new-window -n "Client" "cd $LOVING_AI_WORKSPACE/chatbot-server && python client.py $SLACK_BOTNAME localhost; $SHELL"
-tmux attach
