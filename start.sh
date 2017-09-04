a74811714c43ce7836790e4dd24a3e58caf982b8
Amen Belayneh <amenbelayneh@gmail.com>
2017-09-04 13:16:03 +0800

    ignore log files

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ sed -i -e 's/^$host = "1.22.08.4"/$host = "localhost"/g' \
   ChatScript-engine/WEBINTERFACE/SPEECH/ui.php
 
 # Start the server.
-nohup caddy > /dev/null 2>&1 > caddy.logs &
+nohup caddy > /dev/null 2>&1 > caddy.log &
 
 # Start the chatscript engine.
 cd ChatScript-engine && ./run.sh --users ../users --logs ../logs --topic ../topic --tmp ../tmp -p 1024
